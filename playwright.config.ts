7d540ccba8f002da0b8247262362d173a4d91025
Phelype <phelype@systhefactory.com.br>
2025-06-12 13:31:31 -0300

    News updates with ci/cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,14 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
   use: {
+    baseURL: 'https://sgcweb.construtoralaterza.com.br',
     headless: true,
-    screenshot: 'only-on-failure', // já tira screenshot no erro automaticamente
+    screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
   reporter: [
     ['html', { open: 'never' }]
   ],
+
+  
 });
